7529f96195b00c95497a78fb20139d602d95a8b7
Carlos Sanz Garcia <studiocam6@yahoo.es>
2017-12-14 11:54:47 +0100

    [feature] Improved main.ts initialize file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import AppComponent from './app/AppComponent';
+import ToolbarComponent from './toolbar/ToolbarComponent';
 
-new AppComponent(document.getElementById('app'));
+const $app = document.getElementById('app');
+if ($app) {
+  // tslint:disable-next-line:no-unused-expression
+  new AppComponent($app);
+} else {
+  console.error('no element #app found, the app cannot be initialized');
+}
