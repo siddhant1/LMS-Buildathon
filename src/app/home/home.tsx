7ff96616981bc534e1d1584fa2306ca3bc55c2f2
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-04-19 03:13:52 +0530

    changes-nik

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -3,45 +3,52 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <img src="../../../assets/images/Capture.PNG" style={{width: '200vh', height:70}} alt=""/>
+<div>
+          <img
+            src="../../../assets/images/Capture.PNG"
+            style={{ width: "200vh", height: 70 }}
+            alt=""
+          />
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "row",
-          marginTop: 270
+          display: "grid",
+          gridTemplateColumns: '1fr 1fr 1fr',
+          justifyContent: 'space-between',
+          marginTop: 270,
+          // verticalRule:'dotted',
+          marginLeft: 370,
+          width: '50%'
         }}
       >
-        <Link to="/scribble" style={{ width: 100, height: 100, padding: 20 }}>
+        <Link to="/scribble" style={{ width: 100, height: 100, paddingRight: 20,paddingLeft: 20 }}>
           <img
             src="../../../assets/images/Artboard – 2.png"
             alt=""
             style={{ width: 100, height: 100 }}
           />
         </Link>
-        <Link to="/webcam" style={{ width: 100, height: 100, padding: 20 }}>
+        <Link to="/webcam" style={{ width: 100, height: 100, paddingRight: 20 }}>
           <img
             src="../../../assets/images/Artboard – 1.png"
             alt=""
             style={{ width: 100, height: 100 }}
           />
         </Link>
-        <Link to="/screenrec" style={{ width: 100, height: 100, padding: 20 }}>
+        <Link to="/screenrec" style={{ width: 100, height: 100, paddingRight: 20 }}>
           <img
             src="../../../assets/images/Artboard – 3.png"
             alt=""
             style={{ width: 100, height: 100 }}
           />
         </Link>
-        <Link to="/scribble" style={{ width: 100, height: 100, padding: 20 }}>
+        <Link to="/scribble" style={{ width: 100, height: 100, paddingRight: 20}}>
           <img
             src="../../../assets/images/Artboard – 4.png"
             alt=""
             style={{ width: 100, height: 100 }}
           />
         </Link>
-        <Link to="/scribble" style={{ width: 100, height: 100, padding: 20 }}>
+        <Link to="/scribble" style={{ width: 100, height: 100, paddingRight: 20,paddinghLeft: 20 }}>
           <img
             src="../../../assets/images/Artboard – 5.png"
             alt=""
@@ -49,7 +56,8 @@ const Home = () => {
           />
         </Link>
       </div>
-    </div>
+      </div>
+  
   );
 };
 
