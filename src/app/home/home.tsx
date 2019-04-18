a73a1d4dbe0e462b37c4df20322e012f60e0d5fc
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-04-19 02:46:48 +0530

    frontend first page

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -3,11 +3,53 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
-      <Link to="/scribble">scribble</Link>
-      <Link to="/screenrec">screen rec</Link>
-      <Link to="/webcam">webcam</Link>
-    </>
+    <div>
+      <img src="../../../assets/images/Capture.PNG" style={{width: '200vh', height:70}} alt=""/>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          marginTop: 270
+        }}
+      >
+        <Link to="/scribble" style={{ width: 100, height: 100, padding: 20 }}>
+          <img
+            src="../../../assets/images/Artboard – 2.png"
+            alt=""
+            style={{ width: 100, height: 100 }}
+          />
+        </Link>
+        <Link to="/webcam" style={{ width: 100, height: 100, padding: 20 }}>
+          <img
+            src="../../../assets/images/Artboard – 1.png"
+            alt=""
+            style={{ width: 100, height: 100 }}
+          />
+        </Link>
+        <Link to="/screenrec" style={{ width: 100, height: 100, padding: 20 }}>
+          <img
+            src="../../../assets/images/Artboard – 3.png"
+            alt=""
+            style={{ width: 100, height: 100 }}
+          />
+        </Link>
+        <Link to="/scribble" style={{ width: 100, height: 100, padding: 20 }}>
+          <img
+            src="../../../assets/images/Artboard – 4.png"
+            alt=""
+            style={{ width: 100, height: 100 }}
+          />
+        </Link>
+        <Link to="/scribble" style={{ width: 100, height: 100, padding: 20 }}>
+          <img
+            src="../../../assets/images/Artboard – 5.png"
+            alt=""
+            style={{ width: 100, height: 100 }}
+          />
+        </Link>
+      </div>
+    </div>
   );
 };
 
